98c8a56ddd0988b2f00a1fe9ef7c21fc7f80a16e
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-04 15:44:04 +0200

    feat: offload payment push to a channel

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::sync::Arc;
 
 #[cfg(feature = "perf")]
 use pprof::flamegraph::Options;
+use rinha_de_backend::domain::payment::Payment;
 use rinha_de_backend::infrastructure::config::settings::Config;
+use rinha_de_backend::infrastructure::queue::redis_payment_queue::PaymentQueue;
+use rinha_de_backend::infrastructure::workers::mpsc_to_redis_worker::mpsc_to_redis_worker;
 use rinha_de_backend::run;
+use rinha_de_backend::use_cases::create_payment::CreatePaymentUseCase;
+use tokio::sync::mpsc;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -15,7 +20,20 @@ async fn main() -> std::io::Result<()> {
 		.unwrap();
 
 	let config = Arc::new(Config::load().expect("Failed to load configuration"));
-	let result = run(config.clone()).await;
+
+	let redis_client =
+		redis::Client::open(config.redis_url.clone().as_ref()).unwrap();
+	let payment_queue = PaymentQueue::new(redis_client.clone());
+	let create_payment_use_case = CreatePaymentUseCase::new(payment_queue.clone());
+
+	let (payment_sender, payment_receiver) = mpsc::channel::<Payment>(100_000);
+
+	tokio::spawn(mpsc_to_redis_worker(
+		payment_receiver,
+		create_payment_use_case.clone(),
+	));
+
+	let result = run(config.clone(), payment_sender).await;
 
 	#[cfg(feature = "perf")]
 	if let Ok(report) = guard.report().build() {
